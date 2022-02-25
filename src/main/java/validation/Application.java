19b20d1b4b7d823a4780d33fd14c6bd06e0fa7e1
User <glazdunovao@mail.ru>
2022-02-25 20:50:04 +0300

    practice 1

---
+++ src/main/java/validation/Application.java
@@ -0,0 +1,12 @@
+package validation;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
+    SpringApplication.run(Application.class);
+}
+}
+
+
